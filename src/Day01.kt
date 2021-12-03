c8c33106941e951347c5132788fb74ef42d8e5ff
Talha Munir <46424469+mtalham@users.noreply.github.com>
2021-12-03 23:59:30 +0100

    finished part1 and part2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,12 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+  fun part1(input: List<Int>) = input.windowed(2)
+    .count { (first, second) -> first < second }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+  fun part2(input: List<Int>): Int = input.windowed(4)
+    .count { it.first() < it.last() }
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+  val input = readIntInput("Day01")
+  println(part1(input))
+  println(part2(input))
 }
