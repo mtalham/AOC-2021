7ee796c118a468797ca3239649928200f3e6ff46
Talha Munir <46424469+mtalham@users.noreply.github.com>
2021-12-07 18:50:14 +0100

    finished part2 of day03

--- src/Day03.kt
+++ src/Day03.kt
@@ -18,11 +18,37 @@ fun main() {
     return gamma.toInt(2) * epsilon.toInt(2)
   }
 
+  fun rating(input: List<String>, isOxygen: Boolean): String {
+    var candidates = input
+    for (column in 0 until input[0].length) {
+      val (zeros, ones) = candidates.countBitsInColumn(column)
+      val mostCommon = if (zeros > ones) '0' else '1'
+      candidates = candidates.filter {
+        when (isOxygen) {
+          true -> it[column] == mostCommon
+          false -> it[column] != mostCommon
+        }
+      }
+      if (candidates.size == 1) break
+    }
+    return candidates.single()
+  }
 
-  fun part2(input: List<Int>): Int = input.windowed(4)
-    .count { it.first() < it.last() }
+  fun part2(input: List<String>): Int {
+    return rating(input, true).toInt(2) * rating(input, false).toInt(2)
+  }
+
 
   val input = readInput("Day03")
   println(part1(input))
-//  println(part2(input))
+  println(part2(input))
+}
+
+private fun List<String>.countBitsInColumn(column: Int): Pair<Int, Int> {
+  var zeros = 0
+  var ones = 0
+  for (line in this) {
+    if (line[column] == '0') zeros++ else ones++
+  }
+  return zeros to ones
 }
